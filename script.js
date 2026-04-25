890b9f0c81dd8148e4bcd6a04d234c080909d133
HoneyBounes <95234742+HoneyBounes@users.noreply.github.com>
2026-04-25 22:40:46 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function updateTimer() {
         const minutes = Math.floor((seconds % (60 * 60)) / 60);
         const secs = seconds % 60;
 
-        timer = `...и то ${days} дана ${hours} сати ${minutes} минута ${secs} секунди! <3`;
+        timer = `...и то већ ${days} дана ${hours} сати ${minutes} минута ${secs} секунди!`;
     }
 
     document.getElementById("timer").textContent = timer;
