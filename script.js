5d5019266b3dd3e665553bfc8d814ce3fb6b3b58
HoneyBounes <95234742+HoneyBounes@users.noreply.github.com>
2026-04-25 22:45:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function updateTimer() {
         const minutes = Math.floor((seconds % (60 * 60)) / 60);
         const secs = seconds % 60;
 
-        timer = `...и то већ ${days} дана ${hours} сати ${minutes} минута ${secs} и секунди!`;
+        timer = `...и то већ ${days} дана ${hours} сати ${minutes} минута и ${secs} секунди!`;
     }
 
     document.getElementById("timer").textContent = timer;
